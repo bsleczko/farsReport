4913edb14819f2eed90691d725b80f1a0e4dfb6e
Bartek Sleczkowski <b.sleczkowski@gmail.com>
2018-10-31 14:38:30 +0100

    Improv test

--- tests/testthat/test_map_state.R
+++ tests/testthat/test_map_state.R
@@ -1,5 +1,5 @@
 testthat::test_that("FARS map plotting function",
                     {
-                      testthat::expect_null(farsReport:::fars_map_state())
+                      testthat::expect_null(farsReport:::fars_map_state(state.num = 1, year = 2013))
                     }
 )
